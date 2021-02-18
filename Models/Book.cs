fd531e91c1cbd918d82681f1494ec769b03b488b
bensoncraft <71245174+bensoncraft@users.noreply.github.com>
2021-02-17 23:46:59 -0700

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string ISBN { get; set; }
+        public string ClassificationCategory { get; set; }
+        public float Price { get; set; }
+
+    }
+}
---
+++ Models/EFBooksRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Models
+{
+    public class EFBooksRepository : IBooksRepository
+    {
+        private BooksDbContext _context;
+
+        //Constructor
+        public EFBooksRepository (BooksDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Book> Books => _context.Books;
+
+    }
+}
